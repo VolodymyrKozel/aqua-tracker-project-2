df89f5f91cbcf99fa6354091a4ea9f8f7827bdfd
Volodymyr <Volodumurvel@gmail.com>
2024-07-10 09:19:24 +0200

    add fonts, favicon ...

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -10,7 +10,7 @@ import { selectIsLoggedIn } from '../redux/auth/selectors';
 export const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
   /* const isLoggedIn = useSelector(selectIsLoggedIn); */
 
-  const isLoggedIn = false;
+  const isLoggedIn = true;
 
   return isLoggedIn ? Component : <Navigate to={redirectTo} replace />;
 };
