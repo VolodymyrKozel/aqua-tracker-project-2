0ac2edae8b4bec07a6a125b25c1ddbc99fd745f6
Volodymyr <Volodumurvel@gmail.com>
2024-07-10 06:39:36 +0200

    delete

--- src/components/AdvantagesSection/AdvantagesSection.jsx
+++
@@ -1,6 +0,0 @@
-import css from './AdvantagesSection.module.css';
-function AdvantagesSection() {
-  return <section className={css.container}>AdvantagesSection</section>;
-}
-
-export default AdvantagesSection;
--- src/pages/NotFoundPage/NotFoundPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function NotFoundPage() {
-  return <div>NotFoundPage</div>;
-}
-
-export default NotFoundPage;
