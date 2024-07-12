3c8197521a0162df905eb10d2f2bd90542c218e0
IhorSlakva <igorek.slakva@gmail.com>
2024-07-12 19:44:08 +0300

    update

--- src/components/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/AddWaterBtn/AddWaterBtn.jsx
@@ -1,5 +1,16 @@
+import css from './AddWaterBtn.module.css';
+
 const AddWaterBtn = () => {
-  return <div>AddWaterBtn</div>;
+  return (
+    <div>
+      <button type="button" className={css.btn}>
+        <svg className={css.icon} width={12} height={12}>
+          <use href="/src/assets/images/icons.svg#icon-glass-fill"></use>
+        </svg>
+        Add water
+      </button>
+    </div>
+  );
 };
 
 export default AddWaterBtn;
