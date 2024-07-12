ed505a7499a1cc07a32c9c72b3f161b02b952f45
IhorSlakva <igorek.slakva@gmail.com>
2024-07-12 12:35:41 +0300

    update

--- src/components/WaterDailyNorma/WaterDailyNorma.jsx
+++ src/components/WaterDailyNorma/WaterDailyNorma.jsx
@@ -1,9 +1,11 @@
 import css from './WaterDailyNorma.module.css';
 
 const WaterDailyNorma = () => {
+  const dailyNorma = 1.5;
+
   return (
     <div className={css.container}>
-      <p className={css.volume}>1.5 L</p>
+      <p className={css.volume}> {dailyNorma ? `${dailyNorma}` : '0'} L</p>
       <p className={css.text}>My daily norma</p>
     </div>
   );
