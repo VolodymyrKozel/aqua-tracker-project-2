ee5c58bcc528f2330aae2d566e02ff6faa220e23
Данил <danikkach666@gmail.com>
2024-07-14 15:19:56 +0300

    add corect header modal and close button

--- src/components/UserSettingsForm/images/index.js
+++ src/components/UserSettingsForm/images/index.js
@@ -1,2 +1,3 @@
 export { default as ava } from './ava.jpg';
 export { default as ava2x } from './ava2x.jpg';
+export { default as avatar_photo_default } from './avatar_photo_default.jpg';
--- src/components/UserSettingsModal/UserSettingsModal.jsx
+++ src/components/UserSettingsModal/UserSettingsModal.jsx
@@ -1,13 +1,21 @@
-// import Icon from '../shared/Icon/Icon';
+import Icon from '../shared/Icon/Icon';
 import UserSettingsForm from '../UserSettingsForm/UserSettingsForm';
 
 import css from './UserSettingsModal.module.css';
 
-export default function UserSettingsModal() {
+export default function UserSettingsModal({ closeModal }) {
   return (
-    <div>
-      <h1 className={css.formTitle}>Setting</h1>
-      {/* <Icon className={css.icon} width={24} height={24} id={'icon-cross'} /> */}
+    <div className={css.container}>
+      <div className={css.nav}>
+        <h1 className={css.formTitle}>Setting</h1>
+        <Icon
+          onClick={closeModal}
+          className={css.icon}
+          width={24}
+          height={24}
+          id={'icon-cross'}
+        />
+      </div>
 
       <UserSettingsForm />
     </div>
