75d4566d168cfdcf033864c1268165cc8041dd73
AnnaP-G <klientkv1511@gmail.com>
2024-07-14 16:41:08 +0300

    added userBar logic

--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -1,22 +1,33 @@
-import { useDispatch, useSelector } from 'react-redux';
 import Button from '../shared/Button/Button.jsx';
 import UserBarPopover from '../UserBarPopover/UserBarPopover.jsx';
 import { IconChevronDown } from './IconChevronDown.jsx';
-import { togglePopover } from '../../redux/popover/slice.js';
 import css from './UserBar.module.css';
-import { selectIsVisible } from '../../redux/popover/selector.js';
 import { IconChevronUp } from './IconChevronUp.jsx';
+import { useEffect, useRef, useState } from 'react';
 
 const UserBar = () => {
-  const dispatch = useDispatch();
-  const isPopoverOpen = useSelector(selectIsVisible);
+  const [togglePopover, setTogglePopover] = useState(false);
+  const popoverRef = useRef(null);
 
-  const handleButtonClick = () => {
-    dispatch(togglePopover());
+  const handleOutsideClick = e => {
+    if (popoverRef.current && !popoverRef.current.contains(e.target)) {
+      setTogglePopover(false);
+    }
   };
 
+  const handleButtonClick = () => {
+    setTogglePopover(!togglePopover);
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleOutsideClick);
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, []);
+
   return (
-    <div className={css.userBar}>
+    <div className={css.userBar} ref={popoverRef}>
       <Button
         onClick={handleButtonClick}
         variant="secondary"
@@ -28,14 +39,14 @@ const UserBar = () => {
           src="https://cdn.iconscout.com/icon/free/png-256/free-avatar-373-456325.png"
           alt="Avatar"
         />
-        {isPopoverOpen ? (
+        {togglePopover ? (
           <IconChevronUp className={css.iconChevronUp} />
         ) : (
           <IconChevronDown className={css.iconChevronDown} />
         )}
       </Button>
 
-      {isPopoverOpen && <UserBarPopover onClose={handleButtonClick} />}
+      {togglePopover && <UserBarPopover onClose={handleButtonClick} />}
     </div>
   );
 };
