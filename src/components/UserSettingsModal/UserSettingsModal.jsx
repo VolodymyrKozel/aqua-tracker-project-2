dcfc135750ff6a097703e92ecda25ab3440fa3b8
Данил <danikkach666@gmail.com>
2024-07-14 13:27:37 +0300

    add corect icon close

--- src/components/UserSettingsModal/UserSettingsModal.jsx
+++ src/components/UserSettingsModal/UserSettingsModal.jsx
@@ -1,4 +1,4 @@
-import Icon from '../shared/Icon/Icon';
+// import Icon from '../shared/Icon/Icon';
 import UserSettingsForm from '../UserSettingsForm/UserSettingsForm';
 
 import css from './UserSettingsModal.module.css';
@@ -7,7 +7,7 @@ export default function UserSettingsModal() {
   return (
     <div>
       <h1 className={css.formTitle}>Setting</h1>
-      <Icon className={css.icon} width={24} height={24} id={'icon-cross'} />
+      {/* <Icon className={css.icon} width={24} height={24} id={'icon-cross'} /> */}
 
       <UserSettingsForm />
     </div>
