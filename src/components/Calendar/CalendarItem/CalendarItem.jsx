08799b278526d2c1440d8a50bc74768a8fd5da4f
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-07-14 23:22:30 +0300

    month-info-render-without-backend-data

--- src/components/Calendar/CalendarItem/CalendarItem.jsx
+++ src/components/Calendar/CalendarItem/CalendarItem.jsx
@@ -1,8 +1,8 @@
-// import { format, isSameDay } from 'date-fns';
+import { format } from 'date-fns';
 // import css from './CalendarItem.module.css';
 // import clsx from 'clsx';
 
-import List from '../../shared/List/List';
+// import List from '../../shared/List/List';
 
 // function CalendarItem({ day, currentDate }) {
 //   const isToday = isSameDay(day, currentDate);
@@ -18,19 +18,17 @@ import List from '../../shared/List/List';
 
 // export default CalendarItem;
 
-const CalendarItem = ({ data, className }) => {
+const CalendarItem = ({ data, setSelectedDate }) => {
+  const handleClickDay = () => {
+    setSelectedDate(data.day);
+  };
   return (
-    <div className={className}>
-      {/* <h2>{data.month}</h2> */}
-      <div className="days-container">
-        {Object.entries(data.days).map(([day, value]) => (
-          <List key={day} className="day-item">
-            <li className="day-number">{day}</li>
-            <li className="day-value">{value}</li>
-          </List>
-        ))}
-      </div>
-    </div>
+    <li>
+      <button onClick={handleClickDay}>
+        <p>{format(data.day, 'd')}</p>
+        <span>{data.value}</span>
+      </button>
+    </li>
   );
 };
 
