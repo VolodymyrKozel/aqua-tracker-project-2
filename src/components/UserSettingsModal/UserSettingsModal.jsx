f53c69d2c0d4f8cf6a40e076e638b6eba17467c1
Данил <danikkach666@gmail.com>
2024-07-17 20:14:30 +0300

    add corect css modal

--- src/components/UserSettingsModal/UserSettingsModal.jsx
+++ src/components/UserSettingsModal/UserSettingsModal.jsx
@@ -5,18 +5,28 @@ import css from './UserSettingsModal.module.css';
 
 export default function UserSettingsModal({ closeModal }) {
   return (
-    <div className={css.container}>
-      <div className={css.nav}>
-        <h1 className={css.formTitle}>Setting</h1>
-        <Icon
-          onClick={closeModal}
-          className={css.icon}
-          width={24}
-          height={24}
-          id={'icon-cross'}
-        />
-      </div>
-
+    // <div className={css.container}>
+    //   <div className={css.nav}>
+    //     <h1 className={css.formTitle}>Setting</h1>
+    //     <Icon
+    //       onClick={closeModal}
+    //       className={css.icon}
+    //       width={24}
+    //       height={24}
+    //       id={'icon-cross'}
+    //     />
+    //   </div>
+    //   <UserSettingsForm />
+    // </div>
+    <div className={css.modal}>
+      <h1 className={css.formTitle}>Setting</h1>
+      <Icon
+        onClick={closeModal}
+        className={css.icon}
+        width={24}
+        height={24}
+        id={'icon-cross'}
+      />
       <UserSettingsForm />
     </div>
   );
