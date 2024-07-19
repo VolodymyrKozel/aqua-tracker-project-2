2ce9446369e18b40487460970594ea3561901ff2
Данил <danikkach666@gmail.com>
2024-07-19 19:51:46 +0300

    add button

--- src/components/UserSettingsModal/UserSettingsModal.jsx
+++ src/components/UserSettingsModal/UserSettingsModal.jsx
@@ -5,16 +5,15 @@ import css from './UserSettingsModal.module.css';
 export default function UserSettingsModal({ closeModal }) {
   return (
     <div className={css.container}>
-      <div className={css.nav}>
+      <div className={css.modal}>
         <h1 className={css.formTitle}>Setting</h1>
-
-        <Icon
+        <button
           onClick={closeModal}
           className={css.icon}
-          width={24}
-          height={24}
-          id={'icon-cross'}
-        />
+          aria-label="Close modal"
+        >
+          <Icon width={24} height={24} id={'icon-cross'} />
+        </button>
       </div>
 
       <UserSettingsForm />
