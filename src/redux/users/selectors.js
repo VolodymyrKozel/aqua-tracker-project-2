b936ff5fa0851012f9d002203ec1bf87752be65b
IhorSlakva <igorek.slakva@gmail.com>
2024-07-19 12:09:48 +0300

    update water progress bar

--- src/redux/users/selectors.js
+++ src/redux/users/selectors.js
@@ -2,5 +2,4 @@ export const selectIsLoggedIn = state => state.users.isLoggedIn;
 export const selectUser = state => state.users.user;
 export const selectIsRefreshing = state => state.users.isRefreshing;
 export const selectIsLoading = state => state.users.isLoading;
-export const selectDailyWaterRate = state => state.users.user.dailyWaterRate;
 export const selectDailyNorma = state => state.users.user.waterDrink;
