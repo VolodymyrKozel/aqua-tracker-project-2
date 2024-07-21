fe4f1f380395ab0157852ed4fe89ab8a33ad9e76
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-07-21 20:34:39 +0300

    calender-update

--- src/components/MonthInfo/MonthInfo.jsx
+++ src/components/MonthInfo/MonthInfo.jsx
@@ -1,35 +1,24 @@
-import { useEffect } from 'react';
 import Calendar from '../Calendar/Calendar';
 import CalendarPagination from '../Calendar/CalendarPagination/CalendarPagination';
 import css from './MonthInfo.module.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { getWaterDataMonthly } from '../../redux/water/operations';
-import { getMonth, getYear } from 'date-fns';
-import { selectDailyNorma } from '../../redux/users/selectors';
-import Loader from '../shared/Loader/Loader';
 /* import { selectDailyNorma } from '../../redux/water/selectors'; */
 
-const MonthInfo = ({ selectedDate, setSelectedDate }) => {
-  /*   const dailyNorma = useSelector(selectDailyWaterRate); */
-  const dispatch = useDispatch();
-  const dailyNorma = useSelector(selectDailyNorma);
-  const isLoading = useSelector(state => state.water.isLoading);
-  useEffect(() => {
-    const month = getMonth(selectedDate) + 1; // Повертає номер місяця (0 - січень, 11 - грудень)
-    const year = getYear(selectedDate); // Повертає рік
-    console.log(isLoading);
-    dispatch(
-      getWaterDataMonthly({ month: month, year: year, dailyNorma: dailyNorma })
-    );
-  }, [dispatch]);
-
+const MonthInfo = ({
+  selectedDate,
+  setSelectedDate,
+  handleNextMonth,
+  handlePrevMonth,
+  setMonthChange,
+}) => {
   return (
     <div className={css.container}>
       <div className={css.header}>
         <h1 className={css.title}>Month</h1>
         <CalendarPagination
           selectedDate={selectedDate}
-          setSelectedDate={setSelectedDate}
+          handleNextMonth={handleNextMonth}
+          handlePrevMonth={handlePrevMonth}
+          setMonthChange={setMonthChange}
         />
       </div>
       <Calendar selectedDate={selectedDate} setSelectedDate={setSelectedDate} />
--- src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
+++ src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
@@ -4,32 +4,67 @@ import css from './WaterDetailedInfo.module.css';
 import MonthInfo from '../MonthInfo/MonthInfo.jsx';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
-import { getWaterDataDay } from '../../redux/water/operations.js';
+import {
+  getWaterDataDay,
+  getWaterDataMonthly,
+} from '../../redux/water/operations.js';
 import { selectDailyNorma } from '../../redux/users/selectors.js';
-import { format } from 'date-fns';
+import { addMonths, format, getMonth, getYear, subMonths } from 'date-fns';
 
 const WaterDetailedInfo = () => {
   const dispatch = useDispatch();
-
-  let dailyNorma = useSelector(selectDailyNorma);
   const [selectedDate, setSelectedDate] = useState(
     format(new Date(), 'yyyy-MM-dd')
   );
-  // can not select daily norma
-  if (!dailyNorma) {
-    dailyNorma = '200';
-  }
+  // попередня дата
+  // const prevDate = useRef(null);
+  const [monthChange, setMonthChange] = useState(null);
+
+  const handlePrevMonth = () => {
+    setSelectedDate(subMonths(selectedDate, 1));
+  };
+  const handleNextMonth = () => {
+    setSelectedDate(addMonths(selectedDate, 1));
+  };
+  const getMonthsData = () => {
+    const month = getMonth(selectedDate) + 1;
+    const year = getYear(selectedDate);
+    dispatch(
+      getWaterDataMonthly({
+        month: month,
+        year: year,
+        dailyNorma: dailyNorma,
+      })
+    );
+  };
+
+  let dailyNorma = useSelector(selectDailyNorma);
+  useEffect(() => {
+    getMonthsData();
+    // prevDate.current = selectedDate;
+  }, [monthChange]);
   useEffect(() => {
     dispatch(getWaterDataDay({ date: selectedDate, dailyNorma: dailyNorma }));
-  }, [dispatch, dailyNorma, selectedDate]);
+    // if (
+    //   prevDate.current !== selectedDate &&
+    //   !isSameMonth(prevDate.current, selectedDate)
+    // ) {
+    //   getMonthsData();
+    // }
+    // prevDate.current = selectedDate;
+  }, [selectedDate]);
+
   return (
     <section className={css.sectionWaterDetailInfo}>
       <div className={css.waterDetailInfoContainer}>
         <UserPanel />
         <DailyInfo selectedDate={selectedDate} />
         <MonthInfo
+          handleNextMonth={handleNextMonth}
+          handlePrevMonth={handlePrevMonth}
           selectedDate={selectedDate}
           setSelectedDate={setSelectedDate}
+          setMonthChange={setMonthChange}
         />
       </div>
     </section>
