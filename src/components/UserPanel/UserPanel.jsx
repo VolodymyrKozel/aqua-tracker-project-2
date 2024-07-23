9d5a058446772649c8c49327dcf09bf0644a5ef0
AnnaP-G <klientkv1511@gmail.com>
2024-07-23 10:27:47 +0300

    change

--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -5,14 +5,17 @@ import { selectUser } from '../../redux/users/selectors.js';
 import { useTranslation } from 'react-i18next';
 
 export const UserPanel = () => {
-  const { t } = useTranslation();
   const user = useSelector(selectUser);
 
   return (
     <div className={css.userPanel}>
       <h2 className={css.userPanelName}>
-        {t('trackerPage.greeting')}
-        <span className={css.userPanelSpan}>, {user.name}</span>
+        Hello,{' '}
+        <span className={css.userPanelSpan}>
+          {user === ''
+            ? user.email.substring(0, user.email.indexOf('@'))
+            : user.name}
+        </span>
       </h2>
       <UserBar user={user} />
     </div>
